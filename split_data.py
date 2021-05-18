fff3513c3170db8e5862464576a24ee3d3d45ea5
Patryk Bartkowiak <patryk.bartkowiak@intel.com>
2021-05-18 08:08:45 +0200

    reduced number of targets

--- split_data.py
+++ split_data.py
@@ -1,5 +1,43 @@
+import os
+import shutil
 import splitfolders
 
-input_folder = "input/wikiart"
-output_folder = "input/processed_data"
-splitfolders.ratio(input_folder, output_folder, seed=1337, ratio=(0.6, 0.2, 0.2))
+input_dir = "input/wikiart"
+output_dir = "input/processed_data"
+
+# there is no "Renaissance" directory to merge all of types so its created
+os.mkdir(input_dir + '/Renaissance')
+
+# merge
+dirs = os.listdir(input_dir)
+
+for dir in [temp for temp in dirs if '_' in temp]:
+    splitted = dir.split('_')
+
+    for word in splitted:
+        if word in dirs:
+            for file in os.listdir(input_dir + '/' + dir):
+                original = input_dir + '/' + dir + '/' + file
+                target = input_dir + '/' + word + '/' + file
+                shutil.move(original, target)
+
+                print(dir + '/' + file + ' -> ' + word + '/' + file)
+
+            os.rmdir(input_dir + '/' + dir)
+            print('Removed ' + dir)
+            break
+
+
+# reduce
+dirs = os.listdir(input_dir)
+
+for dir in dirs:
+    num_of_files = len(os.listdir(input_dir + '/' + dir))
+
+    if num_of_files < 5000:
+        shutil.rmtree(input_dir + '/' + dir)
+        print('Removed ' + dir + ' (< 5000 images)')
+
+# split
+print('Splitting data...')
+splitfolders.ratio(input_dir, output_dir, seed=1337, ratio=(0.6, 0.2, 0.2))
