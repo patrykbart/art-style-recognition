1862fdeb76e82530b586cbd40fda617d13f4f2bd
Patryk Bartkowiak <patryk.bartkowiak@intel.com>
2021-05-21 21:37:16 +0200

    ploting instead of printing

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
+import os
+import cv2
+import random
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import *
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications.resnet50 import preprocess_input
 
+plt.figure(figsize=(12, 6), dpi=80)
 model = load_model('ResNet50_retrained.h5')
-test_data_dir = "input/processed_data/test"
 
-batch_size = 32
 image_size = (224, 224)
 
-train_datagen = ImageDataGenerator(
-    preprocessing_function=preprocess_input,
-    validation_split=0.2
-)
+targets = {0: 'Expressionism', 1: 'Impressionism', 2: 'Realism', 3: 'Renaissance', 4: 'Romanticism'}
 
-test_generator = train_datagen.flow_from_directory(
-    test_data_dir,
-    target_size=image_size,
-    batch_size=batch_size,
-    class_mode='categorical',
-    subset='validation'
-)
+style = random.choice(list(targets.values()))
+files = os.listdir('input/processed_data/test/' + style)
 
-# Final test accuracy
-_, test_acc = model.evaluate(test_generator)
-print('Test accuracy', test_acc)
+path_to_img = 'input/processed_data/test/' + style + '/' + random.choice(list(files))
+print(path_to_img)
+
+img = cv2.imread(path_to_img)
+img = cv2.resize(img, image_size, 3)
+img = np.array(img).astype(np.float32)/255.0
+img = np.expand_dims(img, axis=0)
+
+output = model.predict(img)[0]
+output = [round(prob * 100, 2) for prob in output]
+
+plt.barh(list(targets.values()), output)
+for i, prob in enumerate(output):
+    plt.text(prob + 0.1, i - 0.05, str(prob))
+
+plt.subplots_adjust(left=0.1)
+plt.xlabel('%')
+
+plt.savefig('plot.png')
+plt.show()
