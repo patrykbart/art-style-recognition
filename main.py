c1ca14c6735e465a8591b998b6789b09c771a5b3
Patryk Bartkowiak <patryk.bartkowiak@intel.com>
2021-05-22 10:53:22 +0200

    moved value numbers a little bit

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ class Application(tk.Frame):
 
         ax.barh(list(self.targets.values()), output)
         for i, prob in enumerate(output):
-            ax.text(prob + 0.1, i - 0.05, str(prob))
+            ax.text(prob + 0.1, i - 0.07, str(prob))
 
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
